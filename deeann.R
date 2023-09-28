12d8e9ee0b78e3918cb924e7dd16505194e9c82c
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-09-27 21:42:22 -0400

    Create deeann.R

---
+++ deeann.R
@@ -0,0 +1,33 @@
+head(deeann)
+deeann_control <- subset(deeann, subset = orig.ident == 'control')
+deeann_treated <- subset(deeann, subset = orig.ident == 'treated')
+
+cellchat <- createCellChat(object = deeann_control, group.by = "cell type")
+CellChatDB <- CellChatDB.human
+CellChatDB.use <- CellChatDB
+cellchat@DB <- CellChatDB.use
+cellchat <- subsetData(cellchat)
+cellchat <- identifyOverExpressedGenes(cellchat)
+cellchat <- identifyOverExpressedInteractions(cellchat)
+cellchat <- projectData(cellchat, PPI.human)
+cellchat <- computeCommunProb(cellchat, type = "truncatedMean", trim = 0.2, raw.use = FALSE, population.size = TRUE)
+cellchat <- filterCommunication(cellchat, min.cells = 3)
+cellchat <- computeCommunProbPathway(cellchat)
+cellchat <- aggregateNet(cellchat)
+cellchat <- netAnalysis_computeCentrality(cellchat, slot.name = "netP")
+cellchat_control <- cellchat
+
+cellchat <- createCellChat(object = deeann_treated, group.by = "cell type")
+CellChatDB <- CellChatDB.human
+CellChatDB.use <- CellChatDB
+cellchat@DB <- CellChatDB.use
+cellchat <- subsetData(cellchat)
+cellchat <- identifyOverExpressedGenes(cellchat)
+cellchat <- identifyOverExpressedInteractions(cellchat)
+cellchat <- projectData(cellchat, PPI.human)
+cellchat <- computeCommunProb(cellchat, type = "truncatedMean", trim = 0.2, raw.use = FALSE, population.size = TRUE)
+cellchat <- filterCommunication(cellchat, min.cells = 3)
+cellchat <- computeCommunProbPathway(cellchat)
+cellchat <- aggregateNet(cellchat)
+cellchat <- netAnalysis_computeCentrality(cellchat, slot.name = "netP")
+cellchat_treated <- cellchat
