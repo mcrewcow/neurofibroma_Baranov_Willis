68b6a5b01e6e03a2bc2ab419de9e03bdd405a40c
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-10-04 20:56:31 -0400

    Update deeann_cellchat.R

--- deeann_cellchat.R
+++ deeann_cellchat.R
@@ -39,3 +39,57 @@ ht1 + ht2
 ht1 <- netAnalysis_signalingRole_heatmap(cellchat_treated, pattern = "outgoing")
 ht2 <- netAnalysis_signalingRole_heatmap(cellchat_treated, pattern = "incoming")
 ht1 + ht2
+
+
+strwidth <- function(x) {0.5}
+
+netVisual_chord_cell(cellchat_control, signaling = 'THBS')
+netVisual_chord_cell(cellchat_treated, signaling = 'THBS')
+
+netVisual_chord_cell(cellchat_control, signaling = 'CNTN')
+netVisual_chord_cell(cellchat_treated, signaling = 'CNTN')
+
+netVisual_chord_cell(cellchat_control, signaling = 'PERIOSTIN')
+netVisual_chord_cell(cellchat_treated, signaling = 'PERIOSTIN')
+
+netVisual_chord_cell(cellchat_control, signaling = 'NT')
+netVisual_chord_cell(cellchat_treated, signaling = 'NT')
+
+netVisual_chord_cell(cellchat_control, signaling = 'CD86')
+netVisual_chord_cell(cellchat_treated, signaling = 'CD86')
+
+netVisual_chord_cell(cellchat_control, signaling = 'NRG')
+netVisual_chord_cell(cellchat_treated, signaling = 'NRG')
+
+netVisual_chord_cell(cellchat_control, signaling = 'RELN')
+netVisual_chord_cell(cellchat_treated, signaling = 'RELN')
+
+netVisual_chord_cell(cellchat_control, signaling = 'NECTIN')
+netVisual_chord_cell(cellchat_treated, signaling = 'NECTIN')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'CXCL')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'CXCL')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'CD23')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'CD23')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'CD6')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'CD6')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'ALCAM')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'ALCAM')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'KIT')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'KIT')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'LIFR')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'LIFR')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'LIGHT')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'LIGHT')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'CDH')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'CDH')
+
+netVisual_chord_cell(cellchat_control_no_endo, signaling = 'IL6')
+netVisual_chord_cell(cellchat_treated_no_endo, signaling = 'IL6')
